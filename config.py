8d323ce177d1a23d9e3963d14a7fe4069c0a5c2a
ruchi5134 <kn043613@gmail.com>
2026-07-30 12:47:28 +0530

    Update OWNER_USERNAME in config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
 
         self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
         self.OWNER_ID = int(getenv("OWNER_ID", 0))
-        self.OWNER_USERNAME = getenv("OWNER_USERNAME", "Ankit_sharma_212")
+        self.OWNER_USERNAME = getenv("OWNER_USERNAME", "@lll_Ritik_lll")
         self.OWNER_LINK = f"https://t.me/{self.OWNER_USERNAME.lstrip('@')}"
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 14400)) * 14400
